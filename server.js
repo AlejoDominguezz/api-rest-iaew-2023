8d482c94dcae7d2b9b1f2a00d6483364514d89ca
alumno721 alumno721 <alumno721@labsis-a7m21.labsis.frc.utn.edu.ar>
2023-11-13 19:16:43 -0300

    add: auth0 para apis

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dbConnection from './src/database/config.js';
 import routerPaquetes from './src/routes/paquete.route.js';
+import { auth } from 'express-oauth2-jwt-bearer';
 
 class Server {
 
@@ -10,12 +11,23 @@ class Server {
         this.paths = { 
             paquete:       '/api/paquetes-turisticos'
         }
-
+        
+        this.configurarAutenticacion();
         this.conectarDB();
         this.routes();
     }
     
-
+    configurarAutenticacion() {
+        const jwtCheck = auth({
+          audience: 'http://localhost:3000', // Ajusta esto según tu configuración
+          issuerBaseURL: 'https://dev-ih2kh0g0o06mjqyy.us.auth0.com/', // Ajusta esto según tu configuración
+          tokenSigningAlg: 'RS256', // Ajusta esto según tu configuración
+        });
+    
+        // Aplica el middleware de autenticación a todas las rutas
+        this.app.use(jwtCheck);
+      }
+    
     routes(){ 
         this.app.use(this.paths.paquete , routerPaquetes);
     }
